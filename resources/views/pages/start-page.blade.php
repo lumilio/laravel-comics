77be39a7102a91c992db884857987cc4ff7a35b6
lumilio <logistic-division@lumilium.com>
2021-12-20 01:13:32 +0100

    Setup css scss

--- resources/views/pages/start-page.blade.php
+++ resources/views/pages/start-page.blade.php
@@ -1,6 +1,11 @@
 @extends('layouts.app')
-
 @section('page-title','welcome')
+
+<!-- @section('custom-css')
+<link href="{{asset('css/appN.css')}}" rel="stylesheet">
+@endsection -->
 
 @section('content')
 @endsection
+
+
